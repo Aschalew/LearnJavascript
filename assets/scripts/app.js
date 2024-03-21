1163a8655d7b74a2935220e10b2ff12a84f78933
Aschalew Debebe <kiba.adote2002@gmail.com>
2024-03-21 10:34:46 +0100

    Initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+//import { apiKey } from "./util.js";
+//import { apiKey1, apiKey2 as aa } from "./util.js";
+import * as util from "./util.js";
+
+console.log(util.apiKey1);
+console.log(util.apiKey2);
+console.log(util.default);
---
+++ assets/scripts/util.js
@@ -0,0 +1,4 @@
+export default "saduihidsaoDefault";
+export let apiKey1 = "saduihidsao1";
+export let apiKey2 = "saduihidsao2";
+export let apiKey3 = "saduihidsao3";
